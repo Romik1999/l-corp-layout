79cbbd5514959843be10711cca0b7776fd1551be
roman <usankin.roma@gmail.com>
2023-12-01 15:03:31 +0400

    - finished map - update dist

--- src/js/app.js
+++ src/js/app.js
@@ -101,6 +101,36 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    // Функция ymaps.ready() будет вызвана, когда
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+                center: [53.196256, 50.219040],
+                controls: [],
+                zoom: 13
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'Собственный значок метки',
+                balloonContent: 'Это красивая метка'
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: '../../img/contacts/icon.svg',
+                // Размеры метки.
+                iconImageSize: [30, 42],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-5, -38]
+            });
+
+        myMap.geoObjects
+            .add(myPlacemark)
+    });
+
     // функция для модалки
 
     function calcScroll() {
